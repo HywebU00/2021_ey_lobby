e098ce0e76cabba94d657758d0c2629edb090d9c
Lize Wu <retniw72@gmail.com>
2021-09-03 10:34:47 +0800

    2021-0903 form表單補項目。詳見 密碼4、6

--- lobby/front/js/customize.js
+++ lobby/front/js/customize.js
@@ -197,5 +197,24 @@ $(function() {
         $('.modal_overlay').click(closeModal);
         $('.modal .close').click(closeModal);
     });
+
+    // password_toggle
+    var passShow = false;
+    $('.password_toggle').each(function(index, el) {
+        $(this).find('.btn-icon').off().click(function(e) {
+            if (!passShow) {
+                $(this).children('i').removeClass().addClass('i_show');
+                $(this).parents('.password_toggle').find('input[type="password"]').attr('type', 'text');
+                passShow = true;
+                // console.log(passShow);
+            } else {
+                $(this).children('i').removeClass().addClass('i_hide');
+                $(this).parents('.password_toggle').find('input[type="text"]').attr('type', 'password');
+                passShow = false;
+                // console.log(passShow);
+            }
+            e.preventDefault();
+        });
+    });
     
 });
